c04bdf5230e15ccb1cca6f6eecdb9e04379a5c25
pahntanapat <pahntanapat@gmail.com>
2022-07-15 15:32:55 +0700

    add main

---
+++ main.py
@@ -0,0 +1,39 @@
+import orjson
+import socketio
+import uvicorn
+
+## creates a new Async Socket IO Server
+sio = socketio.AsyncServer(async_mode='asgi', json=orjson)
+## Creates a new Aiohttp Web Application
+app = socketio.ASGIApp(sio, static_files={'/': 'index.html'})
+# app = web.Application()
+# Binds our Socket.IO server to our Web App
+## instance
+# sio.attach(app)
+
+## we can define aiohttp endpoints just as we normally
+## would with no change
+#async def index(request):
+#    with open('index.html') as f:
+#        return web.Response(text=f.read(), content_type='text/html')
+
+
+## If we wanted to create a new websocket endpoint,
+## use this decorator, passing in the name of the
+## event we wish to listen out for
+@sio.event
+async def message(sid, message):
+    ## When we receive a new event of type
+    ## 'message' through a socket.io connection
+    ## we print the socket ID and the message
+    print("Socket ID: ", sid)
+    print(message)
+
+
+## We bind our aiohttp endpoint to our app
+## router
+# app.router.add_get('/', index)
+
+## We kick off our server
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=5000)
